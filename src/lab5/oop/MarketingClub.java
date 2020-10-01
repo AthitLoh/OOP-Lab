afbcf517e8a99d5a5f8d96661a866b087448f77d
coc <coc@172.26.116.130>
2020-10-01 12:33:58 +0700

    add q2 for lab 5

---
+++ src/lab5/oop/MarketingClub.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab5.oop;
+
+/**
+ *
+ * @author coc
+ */
+public class MarketingClub extends Club{
+    protected int budget;
+    
+    public MarketingClub(String c,int m,int b){
+        super(c,m);
+        this.budget = b;       
+    }
+    
+    //public boolean useBudget(int b){
+        
+    //}
+}
---
+++ src/lab5/oop/SportsClub.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab5.oop;
+
+/**
+ *
+ * @author coc
+ */
+public class SportsClub extends Club{
+    public SportsClub(String c, int m){
+        super(c,m);
+    }
+    
+    @Override
+    public int determineBudget() {
+        return (numMember*1000)+(numMember-minNumMember)*100;
+    }
+    
+    //public void ChangeName(String newName) {
+        
+    //}
+}
