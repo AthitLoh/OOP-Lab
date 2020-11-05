591c450aeb59c4f94de580b8c03358ac680239ae
coc <coc@172.26.116.130>
2020-11-05 11:50:54 +0700

    sent the code for lab 8 for modify the triangleTest.java

--- src/lab8/oop/TriangleTest.java
+++ src/lab8/oop/TriangleTest.java
@@ -11,12 +11,12 @@ package lab8.oop;
  */
 public class TriangleTest {
     public static void main(String[] args) {
-        EquilateralTriangle e1 = new EquilateralTriangle(10);
+        EquilateralTriangle e1 = new EquilateralTriangle(5);
         System.out.println(e1.getLargestAngle());
         System.out.println(e1.getLongestSideLength());
         System.out.println(e1.getPerimiter());
 
-        RightTriangle r1 = new RightTriangle(3.0,4.0,5.0);
+        RightTriangle r1 = new RightTriangle(8.0,11.0,3.0);
         System.out.println(r1.getLargestAngle());
         System.out.println(r1.getLongestSideLength());
         System.out.println(r1.getPerimiter());
