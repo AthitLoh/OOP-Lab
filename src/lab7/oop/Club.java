6cb04bfe7054f98e0f9ddf8e5c7ceebd3fd277cb
AthitLoh <69616027+AthitLoh@users.noreply.github.com>
2020-11-19 13:07:31 +0700

    modify Club.java in lab 7

--- src/lab7/oop/Club.java
+++ src/lab7/oop/Club.java
@@ -10,5 +10,37 @@ package lab7.oop;
  * @author coc
  */
 public class Club {
+   // name of the club
+    protected String clubName;
+    // minimum number of members in the club
+    protected int minNumMember;
+    // current number of members
+    protected int numMember;
+
+    public Club(String c, int m) {
+        clubName = c;
+        minNumMember = m;
+        numMember = m;
+    }
+
+    public void addMember(int num) {
+        numMember = numMember+num;
+    }
+
+    // Continue on the right-hand side
+    public void changeName(String newName) {
+        clubName = newName;
+    }
+
+    public String getName() {
+        return clubName;
+    }
+
+    public int determineBudget() {
+        return (numMember*1000);
+    }
     
+    public void advertise() {
+        System.out.println("Please joinclub: "+clubName);
+    } 
 }
