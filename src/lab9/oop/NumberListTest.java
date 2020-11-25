73f35a455ff71a88f3c30930c93bf85548d72820
AthitLoh <69616027+AthitLoh@users.noreply.github.com>
2020-11-25 17:29:39 +0700

    modify NumberListTest.java class in lab 9

--- src/lab9/oop/NumberListTest.java
+++ src/lab9/oop/NumberListTest.java
@@ -22,12 +22,32 @@ public class NumberListTest {
         }        
         try{
             Integer [] array = {4,2,8,7};
-            NumberList n1 = new NumberList(array);
-            n1.setElement(0,8);
-            System.out.println(n1.divideElement(2,0));
+            NumberList n2 = new NumberList(array);
+            n2.setElement(0,8);
+            System.out.println(n2.divideElement(2,0));
         }catch(ArithmeticException n2){
             System.out.println("element can't be 0");
         }
+        
+        try{
+            Integer [] array = {4,2,8,7};
+            NumberList n3 = new NumberList(array);          
+            n3.setElement(-1,8);
+            System.out.println(n3.divideElement(3,0));
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        
+         try{
+            Integer [] array = {4,2,8,7};
+            NumberList n3 = new NumberList(array);
+            n3.setElement(0,8);
+            System.out.println(n3.divideElement(2,0));
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        
+        
         finally{
             System.out.println("All exception handled");
         }
